8b2cb3ed179f8604f576e74cab8a0d446b539d0c
Prakhar Soni <soni.prakhar.04@gmail.com>
2024-10-02 16:35:44 +0530

    Deployment

--- backend/routes/feedback.js
+++ backend/routes/feedback.js
@@ -41,7 +41,7 @@ router.get("/", async (req, res) => {
 });
 
 // GET route to retrieve feedback for the logged-in user
-router.get("/my-feedbacks", checkAuth, async (req, res) => {
+router.get("/my-feedbacks", async (req, res) => {
   console.log(req.session);
   try {
     const feedbacks = await Feedback.find({ userId: req.session.userId });
